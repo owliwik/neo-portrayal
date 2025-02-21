dfc5e074370bc5080f009f54662427e891913676
Owliwik Li <kiwijv.0522@gmail.com>
2025-02-21 11:47:03 +0800

    Update AWS video testing #2

--- app/testing/streaming/[video]/page.tsx
+++ app/testing/streaming/[video]/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 
-import { storage } from '@/lib/supabase/client'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, use } from 'react'
 
-export default function Page() {
+export default function Page({
+  params,
+}: {
+  params: Promise<{ video: string }>
+}) {
+  const { video } = use(params)
   const [videoURL, setVideoURL] = useState(
-    'https://d25vjrhdbnq9g0.cloudfront.net/mac-mini.mp4'
+    `https://d25vjrhdbnq9g0.cloudfront.net/${video}`
   )
 
   useEffect(() => {
@@ -17,7 +21,6 @@ export default function Page() {
       //   console.log(error.message)
       //   return
       // }
-
       // setVideoURL('')
       // console.log(data.signedUrl)
     }
@@ -26,15 +29,11 @@ export default function Page() {
   }, [])
 
   return (
-    <div className='my-20 w-full flex justify-center items-center'>
+    <div className='my-20 w-full flex flex-col justify-center items-center'>
+      <h1 className='text-2xl font-semibold my-10'>Viewing: {video}</h1>
       {videoURL && (
         <div className='h-[40rem] aspect-video flex justify-center items-center'>
-          <video
-            className='w-full h-full'
-            autoPlay
-            controls
-            src={videoURL}
-          />
+          <video className='w-full h-full' autoPlay controls src={videoURL} />
         </div>
       )}
     </div>
